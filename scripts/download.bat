1773554efeccebea6032bbeae3ec7d9e9394e0e9
Thijs Schreijer <thijs@thijsschreijer.nl>
2018-07-10 13:56:12 -0400

    Update Lua 5.3 to newly released 5.3.5

--- scripts/download.bat
+++ scripts/download.bat
@@ -6,7 +6,7 @@ REM  ***************  CUSTOMIZATION SECTION  *********************
 REM relative nameparts with source code
 SET LUA51=lua-5.1.5
 SET LUA52=lua-5.2.4
-SET LUA53=lua-5.3.4
+SET LUA53=lua-5.3.5
 SET LRVERSION=2.4.4
 
 REM Archive suffix to use for all downloads
